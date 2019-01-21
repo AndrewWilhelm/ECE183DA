a58d112626beba8be88c64dc28421ea9ea62dcc6
Aaron Wilhelm <aaron.j.wilhelm@gmail.com>
2019-01-20 16:26:03 -0800

    Adjusted the output to make it easier to copy data to spreadsheets

--- paperbot/Software/vl53l0x_dual_sensor_test/vl53l0x_dual_sensor_test.ino
+++ paperbot/Software/vl53l0x_dual_sensor_test/vl53l0x_dual_sensor_test.ino
@@ -12,7 +12,7 @@
 
 #define SDA_PORT 14
 #define SCL_PORT 12
-VL53L0X sensor;
+VL53L0X sensor; //yellow wire is lidar 1 (D3)
 VL53L0X sensor2;
 
 void setup()
@@ -30,24 +30,24 @@ void setup()
 
   digitalWrite(D3, HIGH);
   delay(150);
-  Serial.println("00");
+  //Serial.println("00");
   
   sensor.init(true);
-  Serial.println("01");
+  //Serial.println("01");
   delay(100);
   sensor.setAddress((uint8_t)22);
 
   digitalWrite(D4, HIGH);
   delay(150);
   sensor2.init(true);
-  Serial.println("03");
+  //Serial.println("03");
   delay(100);
   sensor2.setAddress((uint8_t)25);
-  Serial.println("04");
+  //Serial.println("04");
 
-  Serial.println("addresses set");
+  //Serial.println("addresses set");
   
-  Serial.println ("I2C scanner. Scanning ...");
+  //Serial.println ("I2C scanner. Scanning ...");
   byte count = 0;
 
   for (byte i = 1; i < 120; i++)
@@ -56,30 +56,31 @@ void setup()
     Wire.beginTransmission (i);
     if (Wire.endTransmission () == 0)
     {
-      Serial.print ("Found address: ");
-      Serial.print (i, DEC);
-      Serial.print (" (0x");
-      Serial.print (i, HEX);
-      Serial.println (")");
+      //Serial.print ("Found address: ");
+      //Serial.print (i, DEC);
+      //Serial.print (" (0x");
+      //Serial.print (i, HEX);
+      //Serial.println (")");
       count++;
       delay (1);  // maybe unneeded?
     } // end of good response
   } // end of for loop
-  Serial.println ("Done.");
-  Serial.print ("Found ");
-  Serial.print (count, DEC);
-  Serial.println (" device(s).");
+  //Serial.println ("Done.");
+  //Serial.print ("Found ");
+  //Serial.print (count, DEC);
+  //Serial.println (" device(s).");
 
   delay(3000);
 }
 
 void loop()
 {
-  Serial.print("Lidar 1 range(mm): ");
+  //Serial.print("Lidar 1 range(mm): ");
   Serial.print(sensor.readRangeSingleMillimeters());
+  Serial.print("\t");
   if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
   
-  Serial.print("  Lidar 2 range(mm): ");
+  //Serial.print("  Lidar 2 range(mm): ");
   Serial.println(sensor2.readRangeSingleMillimeters());
   if (sensor.timeoutOccurred()) { Serial.println(" TIMEOUT"); }
 }
